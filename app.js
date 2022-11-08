5596e0a091ae592ea124e10b49d535e15ef0fd0d
Julia <frolovajulia1310@gmail.com>
2022-11-08 16:16:05 +0500

    creating_themes

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 require('@babel/register');
+require('dotenv').config();
+
 const express = require('express');
+
+const mainRouter = require('./routers/mainRouter');
+
+const morgan = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use('/', mainRouter);
 
 app.listen(PORT, () => { console.log(`Server is up, PORT: ${PORT}`); });
